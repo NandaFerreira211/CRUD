fd8ec0ea653125bc5d240dee8c7db60ed44d6185
Maria Fernanda <mariafernandasantosferreira8@gmail.com>
2026-05-28 16:13:34 -0300

    Cria página de cadastro com acesso ao DB

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace CRUD;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public string stringConexao = Environment.GetEnvironmentVariable("MYSQL_STRING");
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(TxtNome.Text) ||
+            string.IsNullOrWhiteSpace(TxtUsername.Text) ||
+            string.IsNullOrWhiteSpace(TxtEmail.Text) ||
+            string.IsNullOrWhiteSpace(TxtSenha.Password))
+        {
+            MessageBox.Show("Todos os campos são obrigatórios.", "Error!");
+            return;
+        }
+
+        using (var conexao = new MySqlConnection(stringConexao))
+        {
+            var query = "INSERT INTO usuarios(nome,username,email,senha)\nVALUES(@nome,@username,@email,@senha)";
+
+            using (var comando = new MySqlCommand(query, conexao))
+            {
+                comando.Parameters.AddWithValue("@nome", TxtNome.Text);
+                comando.Parameters.AddWithValue("@username", TxtUsername.Text);
+                comando.Parameters.AddWithValue("@email", TxtEmail.Text);
+                comando.Parameters.AddWithValue("@senha", TxtSenha.Password);
+
+                try
+                {
+                    conexao.Open();
+
+                    var linhasAfetadas = comando.ExecuteNonQuery();
+                    if (linhasAfetadas > 0) MessageBox.Show("Cadastro realizado com sucesso!");
+                }
+                catch (Exception exception)
+                {
+                    if (exception is MySqlException erroSql)
+                    {
+                        if (erroSql.Number == 1062)
+                        {
+                            MessageBox.Show("O email ou username já foram utilizados");
+                        }
+                    }
+
+                    Console.WriteLine(exception);
+                }
+            }
+        }
+    }
+}
