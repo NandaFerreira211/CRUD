cb61dbcf4b09b088f906342a7c9104d93c64bdea
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-06-15 16:57:06 -0300

    adiciona funcionalidade no botão de curtida

--- Feed.xaml.cs
+++ Feed.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using CRUD.Modelos;
 using MySql.Data.MySqlClient;
 
@@ -6,8 +7,11 @@ namespace CRUD;
 
 public partial class Feed : Window
 {
-    public Feed()
+    private Usuario _usuario;
+    
+    public Feed(Usuario usuario)
     {
+        _usuario = usuario;
         InitializeComponent();
         CarregarPosts_QuandoIniciar();
     }
@@ -61,6 +65,45 @@ public partial class Feed : Window
         catch (Exception e)
         {
             Console.WriteLine(e);
+        }
+    }
+
+    private void BtnCurtir_OnClick(object sender, RoutedEventArgs e)
+    {
+        var botao = (Button)sender;
+        var postagem = (Postagem)botao.Tag;
+        var query = "SELECT 1 FROM curtidas_postagens WHERE usuario_id = @usuario AND postagem_id = @postagem";
+
+        using var conexao = new MySqlConnection(App.StringConexao);
+        using var comando = new MySqlCommand(query, conexao);
+
+        comando.Parameters.AddWithValue("@usuario", _usuario.Id);
+        comando.Parameters.AddWithValue("@postagem", postagem.Id);
+        
+        try
+        {
+            conexao.Open();
+            var leitor = comando.ExecuteReader();
+            string acao;
+            if (leitor.HasRows)
+            {
+                query = "DELETE FROM curtidas_postagens WHERE usuario_id = @usuario AND postagem_id = @postagem";
+                acao = "descurtir";
+            }
+            else
+            {
+                query = "INSERT INTO curtidas_postagens(usuario_id, postagem_id) VALUES (@usuario, @postagem)";
+                acao = "curtir";
+            }
+            conexao.Close();
+            comando.CommandText = query;
+            conexao.Open();
+            var linhasAfetadas = comando.ExecuteNonQuery();
+            if (linhasAfetadas == 0) throw new Exception($"Erro ao {acao} postagem!");
+        }
+        catch (Exception excecao)
+        {
+            MessageBox.Show(excecao.Message);
         }
     }
 }
