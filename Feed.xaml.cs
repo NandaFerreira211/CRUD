e561ada03f42874f342d8e1934c44a80c985d96c
Nandaferreira211 <mariafernandasantosferreira8@gmail.com>
2026-06-17 14:58:48 -0300

    Resgata post curtidos pelo usuário

--- Feed.xaml.cs
+++ Feed.xaml.cs
@@ -8,7 +8,7 @@ namespace CRUD;
 public partial class Feed : Window
 {
     private Usuario _usuario;
-    
+
     public Feed(Usuario usuario)
     {
         _usuario = usuario;
@@ -20,81 +20,85 @@ public partial class Feed : Window
     {
         List<Postagem> listaPostagens = [];
 
-        const string query = "SELECT p.id,\n       p.conteudo,\n       p.curtidas,\n       p.postado_em,\n       u.nome,\n       u.username\nFROM postagens p\n         INNER JOIN usuarios u ON p.usuario_id = u.id\nORDER BY p.postado_em DESC;";
+        const string query =
+            "SELECT p.id,\n       p.conteudo,\n       p.curtidas,\n       p.postado_em,\n       u.nome,\n       u.username,\n       IF(cp.usuario_id IS NOT NULL,TRUE,FALSE) AS curtido\nFROM postagens p\n         INNER JOIN usuarios u ON p.usuario_id = u.id\n      LEFT JOIN  curtidas_postagens cp ON cp.postagem_id = p.id AND CP.usuario_id = @usuario_id\nORDER BY p.postado_em DESC";
 
         using var conexao = new MySqlConnection(App.StringConexao);
 
         using var comando = new MySqlCommand(query, conexao);
-
+        comando.Parameters.AddWithValue("@usuario_id", _usuario.Id);
         try
         {
-          conexao.Open();
+            conexao.Open();
 
-          var leitor = comando.ExecuteReader();
-          if (!leitor.HasRows)
-          {
-              MessageBox.Show("Nenhuma postagem encontrada!");
-              return;
-          }
+            var leitor = comando.ExecuteReader();
+            if (!leitor.HasRows)
+            {
+                MessageBox.Show("Nenhuma postagem encontrada!");
+                return;
+            }
 
-          while (leitor.Read())
-          {
-              var post = new Postagem
-              {
-                  Id = leitor.GetInt32("id"),
-                  Conteudo = leitor.GetString("conteudo"),
-                  Curtidas = leitor.GetInt32("curtidas"),
-                  Postado_em = leitor.GetDateTime("postado_em"),
-                  Usuario = new Usuario
-                  {
-                      Nome = leitor.GetString("nome"),
-                      Username = leitor.GetString("username")
-                  }
-              };
-              listaPostagens.Add(post);
-          }
-          ItemsControlFeed.ItemsSource = listaPostagens;
-          
+            while (leitor.Read())
+            {
+                var post = new Postagem
+                {
+                    Id = leitor.GetInt32("id"),
+                    Conteudo = leitor.GetString("conteudo"),
+                    Curtidas = leitor.GetInt32("curtidas"),
+                    Postado_em = leitor.GetDateTime("postado_em"),
+                    FoiCurtido = leitor.GetBoolean("curtido"),
+                    Usuario = new Usuario
+                    {
+                        Nome = leitor.GetString("nome"),
+                        Username = leitor.GetString("username")
+                    }
+                };
+                listaPostagens.Add(post);
+            }
+
+            ItemsControlFeed.ItemsSource = listaPostagens;
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
-         
         }
     }
 
     private void BtnCurtir_OnClick(object sender, RoutedEventArgs e)
     {
-        var botao=(Button)sender;
+        var botao = (Button)sender;
         var postagem = (Postagem)botao.Tag;
         var query = "SELECT 1 FROM curtidas_postagens WHERE usuario_id =@usuario AND postagem_id =@postagem";
-        
+
         using var conexao = new MySqlConnection(App.StringConexao);
         using var comando = new MySqlCommand(query, conexao);
-        
+
         comando.Parameters.AddWithValue("@usuario", _usuario.Id);
-        comando.Parameters.AddWithValue("@postagem",postagem.Id);
-        
+        comando.Parameters.AddWithValue("@postagem", postagem.Id);
+
         try
         {
-          conexao.Open();
-          var leitor = comando.ExecuteReader();
-          string acao;
-          if (leitor.HasRows)
-          {
-            query = "DELETE FROM curtidas_postagens WHERE usuario_id =@usuario AND postagem_id =@postagem";
-            acao = "descutir";
-          }
-          else
-          {
-             query ="INSERT INTO curtidas_postagens(usuario_id,postagem_id) VALUES(@usuario,@postagem)";
-             acao = "curtir";
-          }
-          conexao.Close();
-          comando.CommandText = query;
-          conexao.Open();
-          var linhasAfetadas = comando.ExecuteNonQuery();
-          if (linhasAfetadas == 0) throw new Exception($"Erro ao {acao} postagem");
+            conexao.Open();
+            var leitor = comando.ExecuteReader();
+            string acao;
+            if (leitor.HasRows)
+            {
+                query = "DELETE FROM curtidas_postagens WHERE usuario_id =@usuario AND postagem_id =@postagem";
+                acao = "descutir";
+                postagem.FoiCurtido = false;
+            }
+            else
+            {
+                query = "INSERT INTO curtidas_postagens(usuario_id,postagem_id) VALUES(@usuario,@postagem)";
+                acao = "curtir";
+                postagem.FoiCurtido = true;
+            }
+
+            conexao.Close();
+            comando.CommandText = query;
+            conexao.Open();
+            var linhasAfetadas = comando.ExecuteNonQuery();
+            if (linhasAfetadas == 0) throw new Exception($"Erro ao {acao} postagem");
         }
         catch (Exception exception)
         {
